6cd224da0dd9ef533c4d7c2ae1c99f3e8117b408
Smitesh <smiteshp2001@gmail.com>
2023-12-27 08:03:56 +0530

    removed unnesesory imports

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
-const exp = require("constants");
 var routes = require("./routes/router");
 
 //configure the application
